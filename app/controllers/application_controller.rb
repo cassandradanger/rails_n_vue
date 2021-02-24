fa6703cf3edf086600e4d8aee5dba2e0122226b8
Casie Siekman <casiesiekman@Casies-MacBook-Pro-2.local>
2021-02-24 09:39:41 -0600

    jwt sessions shizzzzzzz

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+    include JWTsessions::RailsAuthorization
+    rescue_from JWTsessions::Errors::Unauthorized, with :not_authorized
+
+    private
+
+    def current_user
+        @current_user ||= User.find(payload['user_id'])
+    end
+    
+    def not_authorized
+        render json: {error: 'Not authorized'}, status: :unauthorized 
+    end
 end
