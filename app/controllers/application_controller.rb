9dbd50e8f463a29a4faa563314918b807d17408f
Casie Siekman <casiesiekman@Casies-MacBook-Pro-2.local>
2021-02-25 12:51:16 -0600

    up and running no data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::API
-    include JWTsessions::RailsAuthorization
-    rescue_from JWTsessions::Errors::Unauthorized, with :not_authorized
+    include JWTSessions::RailsAuthorization
+    rescue_from JWTSessions::Errors::Unauthorized, with: :not_authorized
 
     private
 
     def current_user
         @current_user ||= User.find(payload['user_id'])
     end
-    
+
     def not_authorized
         render json: {error: 'Not authorized'}, status: :unauthorized 
     end
